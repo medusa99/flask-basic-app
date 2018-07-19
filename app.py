3dc9b00b0842c08210390ab99440163420bb0b2e
medusa99 <tjamesonedm@gmail.com>
2018-07-18 21:12:16 -0700

    Request

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "This is the homepage."
+    return "Method used: %s" % request.method
 
-#/profile/tiff
-@app.route('/profile/<username>')
-def profile(username):
-    return "Hey there %s" % username
-
-#/post/23
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return "<h2>Post ID is %s</h2>" % post_id
-
-if __name__ =="__main__":
+@app.route('/apple', methods=['Get', 'Post'])
+def apple():
+    if request.method == 'Post':
+        return "You are using POST"
+    else:
+        return "Your are probably using GET"
+    
+if  __name__ =="__main__":
     app.run()
