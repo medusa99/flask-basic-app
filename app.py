d4d3a75b1d546a961cd3233336d586a0927134af
medusa99 <tjamesonedm@gmail.com>
2018-07-18 20:36:33 -0700

    Routing. url tied to a return value of a funtion. Use variable in url and specifiy data type for int

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
     return "This is the homepage."
 
-if __name__=="__main__":
-    app.run(debug=True)
+#/profile/tiff
+@app.route('/profile/<username>')
+def profile(username):
+    return "Hey there %s" % username
+
+#/post/23
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "<h2>Post ID is %s</h2>" % post_id
+
+if __name__ =="__main__":
+    app.run()
